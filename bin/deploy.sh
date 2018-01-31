c22113b8290b30e53f26335935ba7deaa6fc6e36
aem <amarkon895@gmail.com>
2018-01-31 11:44:34 -0500

    deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/local/bin/zsh
+# ONLY USE THIS IF YOURE ADAM OR IF YOUVE UPLOADED YOUR
+# SSH KEY TO PANTHEON, OTHERWISE IT WON'T WORK!¡!
+gulp build
+rsync -arLvz --size-only --ipv4 --progress -e 'ssh -p 2222' ./build --temp-dir=~/tmp/ dev.9c5fdfca-3e02-4e9a-84a5-0292b8cf6a5a@appserver.dev.9c5fdfca-3e02-4e9a-84a5-0292b8cf6a5a.drush.in:/srv/bindings/49b3540b3d224a8cb4efaf6713bacbeb/code/wp-content/themes/scout
+echo "Deployed"
+open "https://dev-mentorworks.pantheonsite.io"
