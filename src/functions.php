6136293e128f6a730313cebfcec083900337fdbc
Brian Roth <brianroth5@gmail.com>
2018-02-17 16:55:59 -0500

    Add missing functions.php changes

--- src/functions.php
+++ src/functions.php
@@ -34,7 +34,8 @@ class MentorWorksSite extends TimberSite {
 		$context['foo'] = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
-		$context['menu'] = new TimberMenu('Primary Navigation');
+		$context['primary_menu'] = new TimberMenu('Primary Navigation');
+		$context['secondary_menu'] = new TimberMenu('Secondary Navigation');
 		$context['site'] = $this;
 		return $context;
 	}
