fdbc38a84cf605931c445e02957768dfb0cc9a20
Brian Roth <brianroth5@gmail.com>
2018-02-20 16:26:21 -0500

    Remove php page

--- src/page-for-students.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$context = Timber::get_context();
-
-$forStudentsPageId = 8; // if ID changes, this will have to be manually updated
-$forStudentsPage = new TimberPost($forStudentsPageId);
-$context['forStudentsPage'] = $forStudentsPage;
-
-Timber::render('for-students.twig', $context);
