e6a522510612f934463af66ec71f636e793ff833
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2018-04-11 10:47:59 -0400

    Add hi-fi header WIP*
    - I want to rewrite and clean most of this up, simplify markup, etc.
    - Use Vue instead of jQuery for toggling classes
    - Add WIP transition from hamburger to close (issues)
    - Sign in button needs to change to a link in the mobile menu
    - Other miscellaneous sizing issues in the current version

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,18 @@
 // A simple wrapper for all your custom jQuery;
 // everything in this file will be run on every page
 (function($) {
+  // TODO: can we replace this with Vue? idk
   $(document).ready(function() {
     $('.blog-posts-grid').slick({
       slidesToShow: 3,
       accessibility: false,
     });
-
-    $('[mobile-menu-icon]').on('click', function(e) {
-      e.preventDefault();
-      $('body').toggleClass('mobile-menu--open');
-    });
   });
 })(jQuery);
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    showMenu: false,
+  },
+});
