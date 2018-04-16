5cb41506df70ea44ff6957aec1596f3558e52227
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2018-04-16 15:02:55 -0400

    Remove homepage PHP file and iteratively render components instead

--- src/page-home.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$context = Timber::get_context();
-
-$homePageId = 2563; // if ID changes, this will have to be manually updated
-$homePage = new TimberPost($homePageId);
-$context['homePage'] = $homePage;
-
-Timber::render('home.twig', $context);
