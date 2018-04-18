43c803887175b2ca062bfc2d5480b30402c570ef
Brian Roth <brianroth5@gmail.com>
2018-04-18 13:14:43 -0400

    a lot more

--- src/page-apply.php
+++ src/page-apply.php
@@ -2,4 +2,8 @@
 $post = new TimberPost();
 $context = Timber::get_context();
 $context['post'] = $post;
+$context['recent_blog_posts'] = Timber::get_posts(array(
+  'posts_per_page' => 3
+));
+
 Timber::render('apply.twig', $context);
